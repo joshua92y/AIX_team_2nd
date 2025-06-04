87727a0625fd33e7384d860406b70770caefda70
coconutparty <ssh6947@gmail.com>
2025-06-04 19:06:32 +0900

    feat: AI_Analyzer 상권분석 UI/UX 대폭 개선 - 로딩UI 현실화, 공시지가 원화표시, 핵심지표 색깔통일, API 500에러 수정, 페이지이동 없는 결과표시

---
+++ AI_Analyzer/locai/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class AiAnalyzerConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "locai"
+    verbose_name = "AI Analyzer"
